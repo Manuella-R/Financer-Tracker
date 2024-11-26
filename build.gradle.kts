04c1dc1e7b3bdc31768338ce52c524f582c78ef2
Manuella-R <67600619+Manuella-R@users.noreply.github.com>
2024-11-26 11:24:57 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Project-level build.gradle
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        // This is the correct location for your AGP classpath
+        classpath ("com.android.tools.build:gradle:8.6.0")  // Make sure this is correct
+    }
+}
